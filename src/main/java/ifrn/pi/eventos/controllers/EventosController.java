508a1b9fe5d7361100864f1be71b7ac754d30bdb
Júlia <juliasuenia7@gmail.com>
2026-08-31 22:31:15 -0300

    Criando Repository para Evento

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,13 +1,20 @@
 package ifrn.pi.eventos.controllers;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.EventosRepository;
 
 @Controller
 public class EventosController {
+	
+	@Autowired
+	private EventosRepository er;
+	
 @RequestMapping("/eventos/form")
 	public String form() {
 		return "formEvento";
@@ -17,6 +24,7 @@ public class EventosController {
 	public String adicionar(Evento evento) {
 	
 	System.out.println(evento);
+	er.save(evento);
 	
 	
 		return "evento-adicionado";
