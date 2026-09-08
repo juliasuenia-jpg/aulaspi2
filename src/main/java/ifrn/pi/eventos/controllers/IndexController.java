64a75ab791684027cd018d93f11fbd810fc18dfc
Júlia <juliasuenia7@gmail.com>
2026-09-07 23:14:35 -0300

    Criando página de de detalhes do evento

--- src/main/java/ifrn/pi/eventos/controllers/IndexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/IndexController.java
@@ -10,7 +10,7 @@ public class IndexController {
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("Chamou o método index");
-		return "home";
+		return "redirect:/eventos";
 	}
 
 }
