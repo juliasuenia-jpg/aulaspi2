2f18cb3cc868172181c8a45577142f972245e082
Júlia <juliasuenia7@gmail.com>
2026-09-14 20:14:08 -0300

    Resolve conflitos e atualiza EventosController

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,18 +1,12 @@
 package ifrn.pi.eventos.controllers;
 
 import java.util.List;
-<<<<<<< HEAD
 import java.util.Optional;
-=======
->>>>>>> origin/main
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.PathVariable;
-=======
->>>>>>> origin/main
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,53 +18,43 @@ import ifrn.pi.eventos.repositories.EventosRepository;
 @RequestMapping("/eventos")
 public class EventosController {
 
-	@Autowired
-	private EventosRepository er;
+    @Autowired
+    private EventosRepository er;
 
-	@GetMapping("/form")
-	public String form() {
-		return "eventos/formEvento";
-	}
+    @GetMapping("/form")
+    public String form() {
+        return "eventos/formEvento";
+    }
 
-	@PostMapping
-	public String adicionar(Evento evento) {
-<<<<<<< HEAD
-=======
+    @PostMapping
+    public String adicionar(Evento evento) {
+        System.out.println(evento);
+        er.save(evento);
+        return "eventos/evento-adicionado";
+    }
 
->>>>>>> origin/main
-		System.out.println(evento);
-		er.save(evento);
+    @GetMapping
+    public ModelAndView listar() {
+        List<Evento> eventos = er.findAll();
+        ModelAndView mv = new ModelAndView("eventos/lista");
+        mv.addObject("eventos", eventos);
+        return mv;
+    }
 
-		return "eventos/evento-adicionado";
-	}
+    @GetMapping("/{id}")
+    public ModelAndView detalhar(@PathVariable Long id) {
+        ModelAndView md = new ModelAndView();
+        Optional<Evento> opt = er.findById(id);
 
-	@GetMapping
-	public ModelAndView listar() {
-		List<Evento> eventos = er.findAll();
-		ModelAndView mv = new ModelAndView("eventos/lista");
-		mv.addObject("eventos", eventos);
-		return mv;
-<<<<<<< HEAD
-	}
+        if (opt.isEmpty()) {
+            md.setViewName("redirect:/eventos");
+            return md;
+        }
 
-	@GetMapping("/{id}")
-	public ModelAndView detalhar(@PathVariable Long id) {
-		ModelAndView md = new ModelAndView();
-		Optional<Evento> opt = er.findById(id);
-		if (opt.isEmpty()) {
-			md.setViewName("redirect:/eventos");
-			return md;
-		}
-		md.setViewName("eventos/detalhes");
-		Evento evento = opt.get();
+        md.setViewName("eventos/detalhes");
+        Evento evento = opt.get();
+        md.addObject("evento", evento);
 
-		md.addObject("evento", evento);
-		return md;
-	}
+        return md;
+    }
 }
-=======
-
-	}
-
-}
->>>>>>> origin/main
